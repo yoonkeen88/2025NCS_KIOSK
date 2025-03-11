db25efb5e25ca5581d24efc37f2cd9b86d4b9eda
yoonkeen <agy91521947@gmail.com>
2025-03-11 20:26:52 +0900

    week02 kiosk v0.4

--- week02.py
+++ week02.py
@@ -3,19 +3,28 @@
 # 3) 주문한 음료의 가격을 출력한다.
 price = [2600, 3300, 3600]
 menu = ["Ice Americano", "Ice Latte", "Ice Choco"]   
+total = 0
 
 while True:
     order = int(input(f"주문하세요({menu[0]} {price[0]}원: 1, {menu[1]} {price[1]}원: 2, {menu[2]} {price[2]}원: 3, Exit: 4): "))
 
     if order == 1:
-        print(f"{price[0]}원 입니다.")
+        total += price[0]
+        print(f"{price[0]}원 입니다.\n누적 금액{total}원 입니다.")
+
     elif order == 2:
-        print(f"{price[1]}원 입니다.")
+        total += price[1]
+        print(f"{price[1]}원 입니다.\n누적 금액{total}원 입니다.")
+        
     elif order == 3:
-        print(f"{price[2]}원 입니다.")
+        total += price[2]
+
+        print(f"선택하신 메뉴 {price[2]}원 입니다.\n누적 금액{total}원 입니다.")
+        
     elif order == 4:
-        print(f"서비스를 종료합니다.")
+        print(f"서비스를 종료합니다.\n누적 금액{total}원 입니다.")
+        
         break
     else:
-        print("메뉴에 없습니다.")
+        print("메뉴에 없습니다. \nPlz choose again.")
     
