99061437b9985243a96721bbde36dc383b5fc367
yoonkeen <agy91521947@gmail.com>
2025-03-11 19:16:36 +0900

    first commit

---
+++ week01_Orietation.py
@@ -0,0 +1,16 @@
+print("This is First time of NCS-Application class Hello World")
+# SOLID 원칙
+# s: 단일책임원칙-single responsibility principle : 하나의 클래스는 하나의 책임만 가져야 함
+# o: 개방폐쇄원칙-open closed principle : 확장에는 열려있고 수정에 닫혀있음
+# l: 리스코프치환원칙-liskov substitution principle : 자식클래스는 부모클래스로 대체가능해야함
+# i: 인터페이스분리원칙-interface segregation principle : 클라이언트는 자신이 사용하지 않는 인터페이스에 의존하지 않아야함
+# d: 의존성역전원칙-dependency inversion principle : 고수준모듈은 저수준모듈에 의존하면 안됨
+
+# recursion 재귀함수
+# 재귀함수는 함수가 자기자신을 호출하는 형태
+# 하지만 재귀함수는 함수자체가 메모리에 있는 것을 연산하는 것이기에
+# DP 를 사용해야함.
+
+# DP(Dynamic Programming) 동적계획법
+# 큰 문제를 작은 문제로 나누어 푸는 방법
+
