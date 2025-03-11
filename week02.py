75b5623e1ec29be775daeac8402fc002ab290c9a
yoonkeen <agy91521947@gmail.com>
2025-03-11 20:20:33 +0900

    week02 kiosk v0.3

--- week02.py
+++ week02.py
@@ -1,18 +1,18 @@
 # 1) Ice amricano 2600, Ice latte 3300, Ice choco 3600
 # 2) 아메리카노, 라떼, 초코 중 하나를 입력받는다.
 # 3) 주문한 음료의 가격을 출력한다.
-ame = 2600
-latte = 3300
-choco = 3600
+price = [2600, 3300, 3600]
+menu = ["Ice Americano", "Ice Latte", "Ice Choco"]   
+
 while True:
-    order = int(input("주문하세요(Ice americano: 1, Ice Latte: 2, Ice choco: 3600, Exit: 4): "))
+    order = int(input(f"주문하세요({menu[0]} {price[0]}원: 1, {menu[1]} {price[1]}원: 2, {menu[2]} {price[2]}원: 3, Exit: 4): "))
 
     if order == 1:
-        print(f"{ame}원 입니다.")
+        print(f"{price[0]}원 입니다.")
     elif order == 2:
-        print(f"{latte}원 입니다.")
+        print(f"{price[1]}원 입니다.")
     elif order == 3:
-        print(f"{choco}원 입니다.")
+        print(f"{price[2]}원 입니다.")
     elif order == 4:
         print(f"서비스를 종료합니다.")
         break
