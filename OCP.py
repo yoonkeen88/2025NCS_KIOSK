181234bbaa2f057689a8606b97a86bc073f4ec99
yoonkeen <agy91521947@gmail.com>
2025-03-18 20:28:52 +0900

    week03 OCP v0.2

--- OCP.py
+++ OCP.py
@@ -1,28 +1,40 @@
 import time
+
+## timemeasure decorator
+def timemeasure(func):
+    def wrapper(*args, **kwargs): # 가변인자(많이 오면 많이 담고 아니면 아니고) inner function 
+        s = time.time()
+        result = func(*args, **kwargs)
+        e = time.time()
+        print(f"Elapsed time: {e-s}")
+        return result
+    return wrapper
+# 클로저 문법이 쓰여진 decorator
+
+
 ## oneToN
 def oneToNLoop(n):
     """
     O(n) => linear time
     """
-    s = time.time()
     result = 0
     for i in range(1,n+1):
         result += i
-    e = time.time()
-    print(f"Elapsed time: {e-s}")
     return result
 
+@timemeasure
 def oneToNMath(n):
     """
     O(1) => constant time
     """
-    s = time.time()
     result = n*(n+1)//2
-    e = time.time()
-    print(f"Elapsed time: {e-s}")    
     return result
 
 i = int(input("Enter the number: "))
 
 print(oneToNLoop(i) , "\n")
 print(oneToNMath(i) , "\n")
+
+f = timemeasure(oneToNLoop)
+
+print(f(i))
