f5e27c1a3a22e35b3135e314bc66bab5de9ffe19
yoonkeen <agy91521947@gmail.com>
2025-04-01 18:28:44 +0900

    week05 class-module-user-teacher-student2

--- week05.py
+++ week05.py
@@ -19,3 +19,34 @@ class User:
     
     def __str__(self):
         return f"User: {self.name}, Age: {self.age}"
+    
+class Teacher(User):
+    def __init__(self, name, age, identity, subject):
+        super().__init__(name, age, identity)
+        self.subject = subject
+        self.todo = []
+        self.student = []
+        self.grade = []
+
+    def getTodo(self, todo):
+        """
+        Returns a todo list for the teacher.
+        """
+        self.todo.append(todo)
+        return self.todo
+
+class Student(User):
+    def __init__(self, name, age, identity, grade):
+        super().__init__(name, age, identity)
+        self.grade = grade
+        self.todo = []
+        self.teacher = []
+        self.subject = []
+        self.grade = []
+
+
+# Association
+# has a
+# Aggregation
+# part of
+# Composition
